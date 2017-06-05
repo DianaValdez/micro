e7e1864c20eb5f19c87f4f120f15c392ec1f7795
DianaValdez <diana.rubi.valdez@gmail.com>
2017-06-04 21:26:28 -0600

    Validar el contenido de la post al enviar

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,22 @@
+Template.postSubmit.onCreated(function() {
+
+
+ Session.set('postSubmitErrors', {});
+}); 
+
+Template.postSubmit.helpers({
+ errorMessage: function(field) {	
+	return Session.get('postSubmitErrors')[field];
+	 },
+	errorClass: function (field) {
+	return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+	 }
+   });
+
+
+
+
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -8,6 +27,9 @@ Template.postSubmit.events({
     };
 
 
+    var errors = validatePost(post);   
+    if (errors.title || errors.url)      
+    	return Session.set('postSubmitErrors', errors);
 
     Meteor.call('postInsert', post, function(error, result) { 
          // display the error to the user and abort      
@@ -20,6 +42,6 @@ Template.postSubmit.events({
          
           Router.go('postPage', {_id: result._id});  
     	});
-    	
+
   }
 });
